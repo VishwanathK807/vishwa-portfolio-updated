20bc25ec9c114cd6a0ef6bc393ed71c2ae4db807
Batman-co <vishwanathk807@gmail.com>
2025-09-11 11:35:18 +0530

    next-map

---
+++ src/app/next-sitemap.config.js
@@ -0,0 +1,10 @@
+// next-sitemap.config.js
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://vishwa-portfolio-updated.vercel.app',
+  generateRobotsTxt: true, // optional
+  changefreq: 'weekly',
+  priority: 0.7,
+};
+
+export default config;
